9f0b5e59c68a1183d2257291a784d1de53a06523
Daniel Kimbel <dk@danielkimbel.com>
2021-01-02 15:44:42 -0700

    Add basic flake, renaming and importing config.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Daniel Kimbel's complete, customized NixOS configuration";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/master"; };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      # my personal laptop
+      p51 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./config.nix ];
+      };
+    };
+  };
+}
